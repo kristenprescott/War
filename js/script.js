a190d307537d4921f1eb8b6de8db0546f4aac44b
Kristen Prescott <aello@Pauls-MBP.attlocal.net>
2021-03-23 13:38:30 -0400

    rendered full deck of cards to DOM w header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// DECK
-//build array of suits:
+// DECK:
 const suits = ["spades", "hearts", "clubs", "diamonds"];
-// build array of card values:
 const suitValues = [
   "A",
   "2",
@@ -17,40 +15,47 @@ const suitValues = [
   "Q",
   "K",
 ];
-// declare empty deck array:
-deck = [];
-// make deck function:
+const deck = [];
 const getDeck = () => {
   for (let i = 0; i < suits.length; i++) {
     for (let j = 0; j < suitValues.length; j++) {
-      let card = { valur: suitValues[j], suit: suits[i] };
+      let card = { value: suitValues[j], suit: suits[i] };
       deck.push(card);
     }
   }
 };
-
 getDeck();
 
-// SHUFFLE FUNCTION
-// note: shuffling deck 13x13 times - we'll be swapping around the suitValues(13),  a GOOD shuffle in a real deck is ~12, and I'm adding one for good measure/evenness. 13x13 = 169 ;) nice
+// SHUFFLE DECK:
 const shuffle = (deck) => {
   for (let i = 0; i < 169; i++) {
-    // first position:
     let first = Math.floor(Math.random() * deck.length);
-    // second position:
     let second = Math.floor(Math.random() * deck.length);
-    // temp location to store first position while second position is in its place:
     let temp = deck[first];
 
-    // note: see if I can turn this into the fancy way Mathilda showed us on algorithm friday
-    // swap first with second
     deck[first] = deck[second];
-    // swap second with first value held in temp location
     deck[second] = temp;
   }
 };
 
-console.log(deck);
-shuffle(deck);
-console.log(deck);
-// Success!!
+// shuffle(deck);
+
+// RENDER TO DOM:
+// use placeholder text, add images later
+
+// render deck to DOM:
+const renderDeck = (deck) => {
+  // deck div dom node:
+  const deckContainer = document.getElementById("deck-container");
+  // iterate over deck and make a div.card.card[i] + textContent = card[i] for each card
+  for (let i = 0; i < deck.length; i++) {
+    //create div.card.card[i] for all 52 cards:
+    let cardDiv = document.createElement("div");
+    console.log(cardDiv);
+    cardDiv.classList = `${deck[i].value}${deck[i].suit} card`;
+    cardDiv.textContent = `${deck[i].value}${deck[i].suit}`;
+    // attach to deckContainer
+    deckContainer.appendChild(cardDiv);
+  }
+};
+renderDeck(deck);
