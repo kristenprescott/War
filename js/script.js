19219c316422d24b2d318cfdde6c47b019a95d33
Kristen Prescott <aello@Pauls-MBP.attlocal.net>
2021-03-23 20:45:21 -0400

    created Deck class, gave it the deck and the shuffle method; created Player class

--- js/script.js
+++ js/script.js
@@ -1,61 +1,85 @@
-// DECK:
-const suits = ["spades", "hearts", "clubs", "diamonds"];
-const suitValues = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
-const deck = [];
-const getDeck = () => {
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < suitValues.length; j++) {
-      let card = { value: suitValues[j], suit: suits[i] };
-      deck.push(card);
+//#region CLASSES:
+class Deck {
+  constructor() {
+    this.deck = [];
+
+    const suits = ["spades", "hearts", "clubs", "diamonds"];
+    const ranks = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+
+    // pushes { rank: rank, suit: suit }
+    suits.forEach((suit, s) => {
+      //for each 'suit' item in suits
+      ranks.forEach((rank, r) => {
+        //for eack rank in ranks
+        console.log(r);
+        let card = {
+          rank: ranks[r],
+          suit: suits[s],
+          name: `${rank} of ${suit}`,
+        };
+        this.deck.push(card);
+        console.log(card);
+      });
+    });
+  }
+  //methods
+  shuffle = (deck) => {
+    for (let i = 0; i < 169; i++) {
+      let first = Math.floor(Math.random() * deck.length);
+      let second = Math.floor(Math.random() * deck.length);
+      let temp = deck[first];
+
+      deck[first] = deck[second];
+      deck[second] = temp;
     }
+  };
+}
+
+const newDeck = new Deck();
+newDeck.shuffle();
+
+class Player {
+  constructor(name) {
+    this.name = name;
   }
-};
-getDeck();
+  // methods:
+  sayName = () => {
+    console.log(this.name);
+  };
+}
 
-// SHUFFLE DECK:
-const shuffle = (deck) => {
-  for (let i = 0; i < 169; i++) {
-    let first = Math.floor(Math.random() * deck.length);
-    let second = Math.floor(Math.random() * deck.length);
-    let temp = deck[first];
+const playerOne = new Player("computer");
+const playerTwo = new Player("kristen");
 
-    deck[first] = deck[second];
-    deck[second] = temp;
-  }
-};
+playerOne.sayName();
+playerTwo.sayName();
 
-// shuffle(deck);
+//#endregion CLASSES
 
-// RENDER TO DOM:
-// use placeholder text, add images later
-
-// render deck to DOM:
-const renderDeck = (deck) => {
-  // deck div dom node:
-  const deckContainer = document.getElementById("deck-container");
-  // iterate over deck and make a div.card.card[i] + textContent = card[i] for each card
-  for (let i = 0; i < deck.length; i++) {
-    //create div.card.card[i] for all 52 cards:
-    let cardDiv = document.createElement("div");
-    console.log(cardDiv);
-    cardDiv.classList = `${deck[i].value}${deck[i].suit} card`;
-    cardDiv.textContent = `${deck[i].value}${deck[i].suit}`;
-    // attach to deckContainer
-    deckContainer.appendChild(cardDiv);
-  }
-};
-renderDeck(deck);
+//#region RENDER TO DOM:
+// const renderDeck = (deck) => {
+//   const deckContainer = document.getElementById("deck-container");
+//   for (let i = 0; i < deck.length; i++) {
+//     let cardDiv = document.createElement("div");
+//     console.log(cardDiv);
+//     cardDiv.classList = `${deck[i].value}${deck[i].suit} card`;
+//     cardDiv.textContent = `${deck[i].value}${deck[i].suit}`;
+//     deckContainer.appendChild(cardDiv);
+//   }
+// };
+// renderDeck(newDeck.deck);
+//#endregion RENDER TO DOM
